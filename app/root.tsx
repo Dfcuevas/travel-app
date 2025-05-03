34f50c0a77abad4141119069ca6a0df9160eaf40
Diego Cuevas <ingdcuevas@gmail.com>
2025-05-02 21:08:37 -0500

    routes setup2

--- app/root.tsx
+++ app/root.tsx
@@ -23,6 +23,11 @@ export const links: Route.LinksFunction = () => [
   },
 ];
 
+async () => {
+  const syncfusion = await import("@syncfusion/ej2-base");
+  syncfusion.registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE_KEY);
+};
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
